e1b6a95b562d374555e614642c7ea47b866625bf
takashiii <azufa.fish@gmail.com>
2020-04-22 22:13:08 +0900

    面倒くさかったのでまとめてコミット。表示を絵文字にしました。

--- game.rb
+++ game.rb
@@ -33,6 +33,41 @@ def define(game_mode:, difficulty:, order_to_attack:)
   create_game_with_game_mode(game_mode)
 end
 
+def copy_emoji_array(board)
+  index_x = 0
+  board.map do |line|
+    line.map do |value|
+      index_x += 1
+      emoji = ''
+      if value == '.'
+        case index_x
+        when 1
+          emoji = 'one'
+        when 2
+          emoji = 'two'
+        when 3
+          emoji = 'three'
+        when 4
+          emoji = 'four'
+        when 5
+          emoji = 'five'
+        when 6
+          emoji = 'six'
+        when 7
+          emoji = 'seven'
+        when 8
+          emoji = 'eight'
+        when 9
+          emoji = 'nine'
+        end
+        emoji
+      else
+        value
+      end
+    end
+  end
+end
+
 def create_game_with_game_mode(game_mode)
   if game_mode == 1
     @player1 = Player.new(mark: 'o', board: @board, game_state: @stat)
@@ -46,19 +81,19 @@ def create_game_with_game_mode(game_mode)
   end
 end
 
-def print_board(board)
+def print_board(board, event)
+  emoji_board = copy_emoji_array board
   text = <<-EOS
-  ```
-  ┏━━┳━━┳━━┓
-  ┃ #{board[0][0]} ┃ #{board[0][1]} ┃ #{board[0][2]} ┃
-  ┣━━╋━━╋━━┫
-  ┃ #{board[1][0]} ┃ #{board[1][1]} ┃ #{board[1][2]} ┃
-  ┣━━╋━━╋━━┫
-  ┃ #{board[2][0]} ┃ #{board[2][1]} ┃ #{board[2][2]} ┃
-  ┗━━┻━━┻━━┛
-  ```
+  :#{emoji_board[0][0]}::#{emoji_board[0][1]}::#{emoji_board[0][2]}:
+  :#{emoji_board[1][0]}::#{emoji_board[1][1]}::#{emoji_board[1][2]}:
+  :#{emoji_board[2][0]}::#{emoji_board[2][1]}::#{emoji_board[2][2]}:
   EOS
-  @bot.send_message(@channel, text)
+
+  event.send_embed do |embed|
+    embed.description = text
+    embed.title = "Turn:#{@stat.turn}"
+    embed.color = 0x3BFB00
+  end
 end
 
 def cpu_v_cpu
@@ -83,14 +118,11 @@ def cpu_v_cpu
   end
 end
 
-def p_vs_cpu(x = 0, y = 0)
+def p_vs_cpu(event, x = 0, y = 0)
   if @stat.turn % 2 == @order_to_attack && @is_inputed == false
-    print_board @board.board
+    print_board @board.board, event
     @bot.send_message @channel,
 '置く位置を選んで下さい。
-123
-456
-789
 選んだら１〜９を`o-x!put 4`のように入力して下さい。'
     return
   end
@@ -99,19 +131,16 @@ def p_vs_cpu(x = 0, y = 0)
   begin
     if @stat.turn % 2 == @order_to_attack && @is_inputed
       @player1.put(x: x, y: y)
-      print_board @board.board
+      print_board @board.board, event
     else
       @bot.send_message @channel, 'AIのターン'
       @player2.put(game_board: @board.board) if @difficulty == 1
       @player2.put_softly(game_board: @board.board) if @difficulty == 2
       @is_inputed = false
-      print_board @board.board
+      print_board @board.board, event
       unless @judge.mark_align?('o') || @judge.mark_align?('x') || @judge.all_squares_marked?
         @bot.send_message @channel,
 '置く位置を選んで下さい。
-123
-456
-789
 選んだら１〜９を`o-x!put 4`のように入力して下さい。'
       end
     end
@@ -135,7 +164,7 @@ def p_vs_cpu(x = 0, y = 0)
     init
     return
   end
-  p_vs_cpu if @is_inputed
+  p_vs_cpu event if @is_inputed
 end
 
 def init
@@ -145,7 +174,7 @@ def init
   define(game_mode: @game_mode, difficulty: @difficulty, order_to_attack: @order_to_attack)
 end
 
-def p_vs_p(x, y)
+def p_vs_p(event, x, y)
   x = x.to_i
   y = y.to_i
   begin
@@ -154,7 +183,7 @@ def p_vs_p(x, y)
     else
       @player2.put(x: x, y: y)
     end
-    print_board @board.board
+    print_board @board.board, event
   rescue ArgumentError => e
     @bot.send_message(@channel, '既に置いてあります')
     return
@@ -182,16 +211,8 @@ end
   difficulty_text = ''
   game_mode_text = ''
   order_to_attack_text = ''
-  if difficulty == '1'
-    @difficulty = difficulty.to_i
-    difficulty_text = 'hard'
-  elsif difficulty == '2'
-    @difficulty = difficulty.to_i
-    difficulty_text = 'easy'
-  else
-    event.send_message('入力された難易度が不正です。1か2で入力して下さい。')
-    break
-  end
+  @difficulty = 1
+  @order_to_attack = 1
   if game_mode == '1'
     @game_mode = game_mode.to_i
     game_mode_text = '二人対戦'
@@ -202,26 +223,40 @@ end
     @game_mode = game_mode.to_i
     game_mode_text = 'AI同士が闘う'
   else
-    event.send_message('入力されたゲームモードが不正です。gamemode 1: 2players, 2:single, 3:cpu_vs_cpu')
+    event.send_message('入力されたゲームモードが不正です。gamemode 1: 2players, 2:single')
     break
   end
-  if order_to_attack == '0'
-    @order_to_attack = order_to_attack.to_i
-    order_to_attack_text = '先攻です'
-  elsif order_to_attack == '1'
-    @order_to_attack = order_to_attack.to_i
-    order_to_attack_text = '後攻です'
-  else
-    event.send_message('先攻・後攻は0か1で入力して下さい。0:先攻, 1:後攻')
-    break
+  unless @game_mode == 1
+    if difficulty == '1'
+      @difficulty = difficulty.to_i
+      difficulty_text = 'hard'
+    elsif difficulty == '2'
+      @difficulty = difficulty.to_i
+      difficulty_text = 'easy'
+    else
+      event.send_message('入力された難易度が不正です。1か2で入力して下さい。')
+      break
+    end
+    if order_to_attack == '0'
+      @order_to_attack = order_to_attack.to_i
+      order_to_attack_text = '先攻です'
+    elsif order_to_attack == '1'
+      @order_to_attack = order_to_attack.to_i
+      order_to_attack_text = '後攻です'
+    else
+      event.send_message('先攻・後攻は0か1で入力して下さい。0:先攻, 1:後攻')
+      break
+    end
   end
 
   @is_game_start = true
   @allow_input = false
   event.send_message("User#{event.user.name}がゲームのプレイを開始しました。")
-  event.send_message("ゲームモード：#{game_mode_text}、難易度は#{difficulty_text}、CPUとの戦闘ではプレイヤーは#{order_to_attack_text}")
+  event.send_message("ゲームモード：#{game_mode_text}、難易度は#{difficulty_text}、CPUとの戦闘ではプレイヤーは#{order_to_attack_text}")if @game_mode == 2
+  event.send_message("ゲームモード：#{game_mode_text}") if @game_mode == 1
   define(game_mode: @game_mode, difficulty: @difficulty, order_to_attack: @order_to_attack)
   event.send_message('o-x!start で開始します。')
+  nil
 end
 
 @bot.command :start do |event|
@@ -232,20 +267,18 @@ end
   @already_started = true
   @allow_input = true
   if @game_mode == 1
-    print_board(@board.board)
+    print_board(@board.board, event)
     @bot.send_message @channel,
 '置く位置を選んで下さい。
-123
-456
-789
 選んだら１〜９を`o-x!put 4`のように入力して下さい。'
   elsif @game_mode == 2
     @is_inputed = false
-    p_vs_cpu
+    p_vs_cpu event
   else
     @bot.send_message @channel, 'mode3は未実装です,初期化します'
     init
   end
+  nil
 end
 
 @bot.command :stop do |event|
@@ -254,9 +287,10 @@ end
   @is_game_start = false
   @already_started = false
   define(game_mode: @game_mode, difficulty: @difficulty, order_to_attack: @order_to_attack)
+  nil
 end
 
-@bot.command :put do |_evnet, num|
+@bot.command :put do |event, num|
   break unless @is_game_start && @allow_input
 
   unless num =~ /[1-9]/
@@ -278,12 +312,13 @@ end
   end
 
   if @game_mode == 1
-    p_vs_p(x, y)
+    p_vs_p(event, x, y)
     break
   elsif @game_mode == 2
     @is_inputed = true
-    p_vs_cpu(x, y)
+    p_vs_cpu(event, x, y)
   end
+  nil
 end
 
 @bot.command :help do |event|
